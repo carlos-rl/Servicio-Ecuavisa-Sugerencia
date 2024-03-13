aa9d34121b743d259683ac65a6a52182c0cbd18d
Carlos Rojano <carlosrojano_@hotmail.com>
2024-03-13 10:45:50 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,14 +14,13 @@ var toFormatDates = "YYYY-MM-DD";
 var oracleApi = async function (json = {}) {
 	try {
 		const raw = JSON.stringify({
-		  "_id": "659ecf46f88896ff346d3bbf",
-		  "description": "asasss",
-		  "title": "pruebaa2",
-		  "estado": false,
-		  "meta_existe": true,
-		  "users_suscribed": 0,
-		  "data": [],
-		  "created_at": "2023-04-28T03:43:57.278Z"
+		    "picImg": "",
+		    "description": "Descripcion de prueba sugerencia",
+		    "title": "Prueba 1 sugerencia",
+		    "estado": false,
+		    "meta_existe": true,
+		    "data": [],
+		    "users_suscribed": 0
 		});
 
 		const requestOptions = {
@@ -32,13 +31,17 @@ var oracleApi = async function (json = {}) {
 		  body: raw,
 		  redirect: "follow"
 		};
-		const resp = await fetch("https://bigdata.ecuavisa.com:10001/api/Trx/mdb/ISugerencia", requestOptions);;
+		const resp = await fetch("https://sugerencias-ecuavisa.vercel.app/add", requestOptions);;
 		return await resp.text();
 	} catch (error) {
 		console.log(error);
 		return null;
 	}
 }
+
+routes.get("/", async function (req, res) {
+	return res.json("Servicio de sugerencias: v0.01 - Prueba");
+});
 
 routes.post("/add", async function (req, res) {
 	try {
